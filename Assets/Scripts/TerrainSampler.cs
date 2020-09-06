7f70cd75660e13e3a7ec8427a3f95983325b1777
andreacarrarini <andrea.carrarini.18@gmail.com>
2020-09-06 12:32:37 +0200

    TerrainSampler works properly

--- Assets/Scripts/TerrainSampler.cs
+++ Assets/Scripts/TerrainSampler.cs
@@ -9,15 +9,28 @@ public class TerrainSampler : MonoBehaviour
     private int tileDimension = 1000;                                                               // The tile dimension in world's unit
     private int tileSubdivision = 10;                                                               // How many input vectors a tile contains
     private int inputVectorDimension = 50;                                                          // The dimension and the number of each input vector for the SOM
-    private List<List<Vector3>> inputMatrix;                                                        // The SOM input matrix (list of list of vectors)
+    private Vector3[,] inputMatrix;                                                                 // The SOM input matrix (list of list of vectors)
     private Vector3 pointToSample;                                                                  // The position used to sample the height of each point required
-
-    // TODO:    set the starting point fo sampling as the bottom left angle of the map
+    private Vector3 samplingStartingPoint;
 
     void Start()
     {
-        inputMatrix = new List<List<Vector3>>();
+        GetBottomLeftCorner();
+
+        Sampling();
+
+        foreach ( Vector3 item in inputMatrix )
+        {
+            print( item );
+        }
+    }
+
+    public void Sampling()
+    {
+        inputMatrix = new Vector3[ inputVectorDimension, inputVectorDimension ];
         pointToSample = new Vector3();
+        float xOffset = samplingStartingPoint.x;                                                    // To make it independent from the tile positioning
+        float zOffset = samplingStartingPoint.z;
 
         for ( int i = 0; i < somDimensionInTiles; i++ )
         {
@@ -28,14 +41,29 @@ public class TerrainSampler : MonoBehaviour
                     for ( int jTile = 0; jTile < tileSubdivision; jTile++ )
                     {
                         // SAMPLING
-                        pointToSample.Set( i * tileDimension + iTile * (tileDimension / tileSubdivision), 0f, j * tileDimension + jTile * (tileDimension / tileSubdivision) );
+                        pointToSample.Set( xOffset + i * tileDimension + iTile * (tileDimension / tileSubdivision), 0f,
+                            zOffset + j * tileDimension + jTile * (tileDimension / tileSubdivision) );
 
                         pointToSample.y = Terrain.activeTerrain.SampleHeight( pointToSample );
 
-                        inputMatrix[ i * tileSubdivision + iTile ][ j * tileSubdivision + jTile ] = pointToSample;
+                        inputMatrix[ i * tileSubdivision + iTile, j * tileSubdivision + jTile ] = pointToSample;
                     }
                 }
             }
         }
+    }
+
+    public void GetBottomLeftCorner()                                                               // Find the bottom left corner of the "tilemap"
+    {
+        GameObject mapMagicFather = GameObject.FindGameObjectWithTag( "MapMagic" );                 // The MapMagic object father of all tiles
+        Vector3 bottomLeftCorner = new Vector3( 0f, 0f, 0f );
+
+        foreach ( Transform tileTransform in mapMagicFather.GetComponentInChildren<Transform>() )
+        {
+            if ( tileTransform.position.x <= bottomLeftCorner.x && tileTransform.position.z <= bottomLeftCorner.z )
+                bottomLeftCorner = tileTransform.position;
+        }
+        samplingStartingPoint = bottomLeftCorner;
+        return;
     }
 }
